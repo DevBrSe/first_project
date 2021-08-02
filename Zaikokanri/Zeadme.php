ebcb3db89d765ececb538dbbe16d12fecf97427a
DevBrSe <88287681+DevBrSe@users.noreply.github.com>
2021-08-02 11:45:43 +0900

    Update Zeadme.php

--- Zaikokanri/Zeadme.php
+++ Zaikokanri/Zeadme.php
@@ -10,9 +10,6 @@
 </head>
 <body>
     <h1>Tao mot trang moi tu hom qua </h1>
-<<<<<<< HEAD
-	<p>tao them mot the khac </p>
-=======
->>>>>>> 67682ee1faf227ad101fac8e96dc9b2f79ff2d18
+	<h2>tao them 1 the h2</h2>
 </body>
 </html>
